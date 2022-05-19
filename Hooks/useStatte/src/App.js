9eeef7c5cfa4b5d3bdaf9c57812d5239fe206893
ungnhohoangviet12 <73026405+ungnhohoangviet12@users.noreply.github.com>
2022-05-19 12:16:22 +0700

    update

--- Hooks/useStatte/src/App.js
+++ Hooks/useStatte/src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
-import ColorBox from "./components/colorBox";
+import ColorBox from "./components/ColorBox";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    {id: 1, title: 'I love you Hoang Viet'},
+    {id: 2, title: 'I love you Lan Phuong'},
+    {id: 3, title: 'I love you Ngoc Trinh'},
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index,1);
+    setTodoList(newTodoList)
+  }
+
   return (
     <div className="app">
-      <h1>Wecome to React Hooks</h1>
-      <ColorBox/>
+      <h1>React hooks - TodoList</h1>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
     </div>
   );
 }
