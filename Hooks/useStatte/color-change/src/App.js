374bc8f39b1d4809a8fbd58eccb64e48c547ab36
ungnhohoangviet12 <73026405+ungnhohoangviet12@users.noreply.github.com>
2022-05-19 11:33:47 +0700

    setColor

---
+++ Hooks/useStatte/color-change/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+import ColorBox from "./components/colorBox";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Wecome to React Hooks</h1>
+      <ColorBox/>
+    </div>
+  );
+}
+
+export default App;
