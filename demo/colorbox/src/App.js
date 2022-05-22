aed5f80e46aa3168e901fa77b0308ddefeda26ff
ungnhohoangviet12 <73026405+ungnhohoangviet12@users.noreply.github.com>
2022-05-22 16:01:50 +0700

    update form

--- demo/colorbox/src/App.js
+++ demo/colorbox/src/App.js
@@ -19,11 +19,24 @@ function App() {
       newTodoList.splice(index,  1);
       setTodoList(newTodoList);
     } 
+
+    function handleTodoFormSubmit(formValues) {
+      console.log('Form submit', formValues);
+      // add new todo to current todo list
+      const newTodoList = [...todoList];
+      const newTodo = {
+        id: todoList.length + 1,
+        ...formValues,
+      }
+      newTodoList.push(newTodo);
+      setTodoList(newTodoList);
+    }
+
   return (
     <div className="app">
      <h1>React hooks - To do list</h1>
+     <TodoForm onSubmit={handleTodoFormSubmit}/>
      <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
-     <TodoForm/>
     </div>
   );
 }
--- demo/colorbox/src/components/TodoForm/TodoForm.jsx
+++ demo/colorbox/src/components/TodoForm/TodoForm.jsx
@@ -12,9 +12,30 @@ TodoForm.defaultProps = {
 function TodoForm(props) {
     const { onSubmit } = props;
     const [value, setValue] = useState('');
+
+    function handleValueChange(e) {
+        console.log(e.target.value)
+        setValue(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        // Prevent realoading browser
+        e.preventDefault();
+
+        if(!onSubmit) return;
+
+        const formValues = {
+            title: value,
+        };
+
+        onSubmit(formValues);
+
+        //Reset form
+        setValue('');
+    }
     return (
-        <form>
-            <input type="text" value={value}></input>
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={value} onChange={handleValueChange}></input>
         </form>
     );
 }
